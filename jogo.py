f5968f47f75be0ac1b1cbfa834288033e4758cdb
Guilherme Neves <guigarolim0@gmail.com>
2025-08-17 00:14:55 -0300

    atualizacao

--- jogo.py
+++ jogo.py
@@ -2,6 +2,7 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((720,480))
+clock = pygame.time.Clock()
 running = True
 
 while running:
@@ -9,4 +10,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    screen.fill("purple")
 
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
