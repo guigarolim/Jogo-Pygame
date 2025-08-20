d6a9c7a493771e6efe11f5c1e288751386405119
Guilherme Neves <guigarolim0@gmail.com>
2025-08-19 22:36:14 -0300

    nave que move e barreira

--- jogo.py
+++ jogo.py
@@ -1,19 +1,23 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1000,500))
+screen = pygame.display.set_mode((800,600))
 clock = pygame.time.Clock()
 running = True
 dt = 0 
 player_pos = pygame.Vector2(screen.get_width()/2,screen.get_height()/2)
+nave = pygame.image.load('nave.png').convert()
+nave = pygame.transform.scale(nave, (50,50))
 
 while running:
 
+    screen.fill((0,0,0))
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
 
-    pygame.draw.circle(screen,"green", player_pos, 10)
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player_pos.y -= 300 *dt
@@ -24,8 +28,23 @@ while running:
     if keys[pygame.K_d]:
         player_pos.x += 300 *dt
 
-    pygame.display.flip()
+    nave_rect = nave.get_rect() 
+    nave_rect.center = player_pos
 
+    if nave_rect.left <0:
+        nave_rect.left = 0 
+    if nave_rect.top <0:
+        nave_rect.top = 0
+    if nave_rect.right > screen.get_width():
+        nave_rect.right = screen.get_width()  
+    if nave_rect.bottom > screen.get_height():
+        nave_rect.bottom = screen.get_height()  
+
+    player_pos = pygame.Vector2(nave_rect.center)
+    nave_rect.center = player_pos
+    screen.blit(nave, nave_rect)
+
+    pygame.display.flip()
     dt = clock.tick(60) / 1000
 
 pygame.quit()
